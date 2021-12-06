c1cd76ee857d78c5154d200d315265ec46e93dca
bread-ly <lukas.adelmann@gmail.com>
2021-12-06 17:15:40 +0100

    this it is

--- inventory.js
+++ inventory.js
@@ -27,19 +27,19 @@ function onScanSuccess(decodedText, decodedresult) {
                 switch (true){
                 case (i<10):
                     if (obj.id["id"+group+"000"+"i"].name != undefined){
-                    realdata.push(obj.id["id"+group+"000"+"i"])}
+                    realdata.push("id"+group+"000"+"i")}
                     break;
                 case (i<100):
                     if (obj.id["id"+group+"00"+"i"].name != undefined){
-                    realdata.push(obj.id["id"+group+"00"+"i"])}
+                    realdata.push("id"+group+"00"+"i")}
                     break;
                 case (i<1000):
                     if (obj.id["id"+group+"0"+"i"].name != undefined){
-                    realdata.push(obj.id["id"+group+"0"+"i"])}
+                    realdata.push("id"+group+"0"+"i")}
                     break;
                 case (i<10000):
                     if (obj.id["id"+group+"i"].name != undefined){
-                    realdata.push(obj.id["id"+group+"i"])}
+                    realdata.push("id"+group+"i")}
                     break;
                 }
             }
