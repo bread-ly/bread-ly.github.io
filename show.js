955a60e4453a64f6549d85765dd91b2c20a53b2e
bread-ly <lukas.adelmann@gmail.com>
2021-12-06 19:34:18 +0100

    Wrong Id on normal scan

--- show.js
+++ show.js
@@ -1,15 +1,13 @@
 var obj = JSON.parse(data);
 const geturl = new URLSearchParams(window.location.search);
 const scannedid = geturl.get('k');
-const id = parseInt(scannedid);
-
 
 function ScanPage()
 {
     document.close();
     window.location.replace("index.html?scanned=true");
 }
-document.getElementById("id").innerHTML = id;
-document.getElementById("showname").innerHTML = obj.id["id" + id].name + "<br>";
-document.getElementById("showlocation").innerHTML = obj.id["id" + id].ort + "<br>";
+document.getElementById("id").innerHTML = scannedid;
+document.getElementById("showname").innerHTML = obj.id["id" + scannedid].name + "<br>";
+document.getElementById("showlocation").innerHTML = obj.id["id" + scannedid].ort + "<br>";
 
